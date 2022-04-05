859d3ff5ad9d9ccef003df339b948b7ae6ba8807
s222929 <tugoff25@gmail.com>
2022-04-05 13:01:10 +0200

    2022-04-05 KK

--- scraper.py
+++ scraper.py
@@ -9,7 +9,7 @@ response = requests.get(url)
 page_dom = BeautifulSoup(response.text, "html.parser")
 
 opinions = page_dom.select("div.js_product-review")
-opinion = opinions.pop()
+opinion = opinions.pop(3)
 
 opinion_id = opinion["data-entry-id"]
 author = opinion.select_one("span.user-post__author-name").text.strip()
@@ -18,6 +18,13 @@ score = opinion.select_one("span.user-post__score-count").text.strip()
 content = opinion.select_one("div.user-post__text").text.strip()
 posted_on = opinion.select_one("span.user-post__published > time:nth-child(1)").text.strip()["datetime"]
 bought_on = opinion.select_one("span.user-post__published > time:nth-child(2)").text.strip()["datetime"]
+useful_for = opinion.select_one("button.vote-yes > span").text.strip()
+useful_for = opinion.select_one("button.vote-yes > span").text.strip()
+pros = opinion.select_one("div.review-feature__item--positives ~ div.review-feature__item")
+pros = [item.text.strip() for item in pros]
+cons = opinion.select_one("div.review-feature__item--negatives ~ div.review-feature__item")
+cons = [item.text.strip() for item in cons]
+
 
 print(type(author))
 print(opinion.prettify())
