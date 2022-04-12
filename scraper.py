46a19908b06db0483a00e449871dcfdfa84e3f2d
s222929 <tugoff25@gmail.com>
2022-04-12 12:55:22 +0200

    2022-04-12 KK

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+import json
 import requests
 from bs4 import BeautifulSoup
 
@@ -9,23 +10,39 @@ response = requests.get(url)
 page_dom = BeautifulSoup(response.text, "html.parser")
 
 opinions = page_dom.select("div.js_product-review")
-opinion = opinions.pop(3)
+all_opinions=[]
+for opinion in opinions:
 
-opinion_id = opinion["data-entry-id"]
-author = opinion.select_one("span.user-post__author-name").text.strip()
-rcmd = opinion.select_one("span.user-post__author-name").text.strip()
-score = opinion.select_one("span.user-post__score-count").text.strip()
-content = opinion.select_one("div.user-post__text").text.strip()
-posted_on = opinion.select_one("span.user-post__published > time:nth-child(1)").text.strip()["datetime"]
-bought_on = opinion.select_one("span.user-post__published > time:nth-child(2)").text.strip()["datetime"]
-useful_for = opinion.select_one("button.vote-yes > span").text.strip()
-useful_for = opinion.select_one("button.vote-yes > span").text.strip()
-pros = opinion.select_one("div.review-feature__item--positives ~ div.review-feature__item")
-pros = [item.text.strip() for item in pros]
-cons = opinion.select_one("div.review-feature__item--negatives ~ div.review-feature__item")
-cons = [item.text.strip() for item in cons]
+    opinion_id = opinion["data-entry-id"]
+    author = opinion.select_one("span.user-post__author-name").text.strip()
+    try:
+        rcmd = opinion.select_one("span.user-post__author-name").text.strip()
+    except AttributeError:
+        rcmd = None
+    score = opinion.select_one("span.user-post__score-count").text.strip()
+    content = opinion.select_one("div.user-post__text").text.strip()
+    posted_on = opinion.select_one("span.user-post__published > time:nth-child(1)")["datetime"]
+    bought_on = opinion.select_one("span.user-post__published > time:nth-child(2)")["datetime"]
+    useful_for = opinion.select_one("button.vote-yes > span").text.strip()
+    useless_for = opinion.select_one("button.vote-no > span").text.strip()
+    pros = opinion.select("div.review-feature__item--positives ~ div.review-feature__item")
+    pros = [item.text.strip() for item in pros]
+    cons = opinion.select("div.review-feature__item--negatives ~ div.review-feature__item")
+    cons = [item.text.strip() for item in cons]
 
-
-print(type(author))
-print(opinion.prettify())
+    single_opinion ={
+        "opinion_id": opinion_id.
+        "author": author.
+        "rcmd": rcmd.
+        "content": content.
+        "posted_on": posted_on.
+        "bought_on": bought_on.
+        "useful_for": useful_for.
+        "useless_for": useless_for.
+        "pros": pros.
+        "cons": cons.
+    }
+    all_opinions.append(single_opinion)
+  
+print(json.dumps(single_opinion, intent=1, ensure_ascii=False))
 
